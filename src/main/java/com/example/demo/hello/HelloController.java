5a8ade0e40085d6be48c9103616dca385faed4ac
lucky <lucky@centoscn.vip>
2021-03-13 02:43:36 +0800

    no message

--- src/main/java/com/example/demo/hello/HelloController.java
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @Author: zym
- * @Date: 2021-02-25 14:46
+ * @Author: www.centoscn.vip
+ * @Date: 2021-03-13 02:46
  */
 @RestController
 @RequestMapping
@@ -14,6 +14,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "hello jenkins 2021-02-26 16:28 from qa \n 我提交代码在qa分支，在Gitee进行将qa分支合并到master";
+        return "hello www.centoscn.vip";
     }
 }
